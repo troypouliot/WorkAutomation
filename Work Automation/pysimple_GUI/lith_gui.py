c5d9cd3d63c03fc8a2212a8300d668260e6baf7b
Troy P <troypouliot@yahoo.com>
2021-12-07 13:40:03 -0600

    12/7 update

--- Work Automation/pysimple_GUI/lith_gui.py
+++ Work Automation/pysimple_GUI/lith_gui.py
@@ -1,33 +1,16 @@
 import PySimpleGUI as sg
 import os
-# from pysimple_GUI.lith import lith_oop
-
-#making some edits
-
-
-
-
-
-#######################################################
-
+import io
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.common import exceptions
 from configparser import ConfigParser
-from os import getcwd
+from PIL import Image
 
 
-
-
-
-
-class Lithophane():
+class Lithophane:
     def __init__(self, type, subtype):
-        self.p = {"download.default_directory": "C:\\Users\\troypouliot\\test", "safebrowsing.enabled":"false"}
-        self.op = webdriver.ChromeOptions()
-        self.driver = webdriver.Chrome(options=self.op)
-        self.op.add_experimental_option('prefs', self.p)
+        self.driver = None
         config = ConfigParser()
         config.read('config.ini')
         self.type = type
@@ -45,12 +28,26 @@ class Lithophane():
         self.light_to_lith_dis = config.get(self.subtype, 'light_to_lith_dis')
         self.radius = config.get(self.subtype, 'radius')
         if self.type == 'nl':
-            self.url = 'https://lithophanemaker.com/Night%20Light%20Lithophane.html'
+            self.url = config.get(self.subtype, 'nl_url')
         elif self.type == 'wind':
-            self.url = 'https://lithophanemaker.com/Framed%20Lithophane.html'
+            self.url = config.get(self.subtype, 'window_url')
 
+    def newchromebrowser(self, headless=True, downloadpath=None):
+        """ Helper function that creates a new Selenium browser """
+        options = webdriver.ChromeOptions()
+        if headless:
+            options.add_argument('headless')
+        if downloadpath is not None:
+            prefs = {}
+            # os.makedirs(downloadpath, exist_ok=True)
+            prefs["profile.default_content_settings.popups"] = 0
+            prefs["download.default_directory"] = downloadpath
+            options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(options=options)
+        return self.driver
 
-    def start_browser(self):
+    def start_browser(self, path):
+        self.newchromebrowser(headless=False, downloadpath=path)
         self.driver.get(self.url)
 
     def clear_fields(self):
@@ -61,25 +58,28 @@ class Lithophane():
             except selenium.common.exceptions.InvalidElementStateException:
                 pass
 
-    def dl_wind(self):
-        self.start_browser()
+    def dl_wind(self, file, path):
+        self.start_browser(path)
         self.clear_fields()
         select = Select(self.driver.find_element_by_id('hole_num'))
         select.select_by_visible_text('No Border')
-        self.driver.find_element_by_name('fileToUpload').send_keys(getcwd() + '/test.png')
+        # self.driver.find_element_by_name('fileToUpload').send_keys(getcwd() + '/test.png')
+        self.driver.find_element_by_name('fileToUpload').send_keys(file)
         self.driver.find_element_by_id('lith_res').send_keys(my_lith.res)
         self.driver.find_element_by_id('base_length').send_keys(my_lith.width)
         self.driver.find_element_by_id('height').send_keys(my_lith.height)
         self.driver.find_element_by_id('max_thickness').send_keys(my_lith.max_thick)
         self.driver.find_element_by_id('min_thickness').send_keys(my_lith.min_thick)
         self.driver.find_element_by_id('emailAddress').send_keys(my_lith.user_email)
+        self.driver.find_element_by_id('x_shift').send_keys('0.5')
+        self.driver.find_element_by_id('y_shift').send_keys('0.5')
+        self.driver.find_element_by_id('rect_scale').send_keys('1.0')
         download_btn = self.driver.find_element_by_name('submit')
         download_btn.click()
 
-    def dl_nl(self, file):
-        self.start_browser()
+    def dl_nl(self, file, path):
+        self.start_browser(path)
         self.clear_fields()
-        # self.driver.find_element_by_name('fileToUpload').send_keys(getcwd() + '/test.png')
         self.driver.find_element_by_name('fileToUpload').send_keys(file)
         self.driver.find_element_by_id('lith_res').send_keys(my_lith.res)
         self.driver.find_element_by_id('t_max').send_keys(my_lith.max_thick)
@@ -99,32 +99,28 @@ class Lithophane():
         self.driver.find_element_by_id('rect_scale').send_keys('1.0')
         download_btn.click()
 
-template = 'nl'
-size = 'square'
-
-
-###################################################################
-
-
-
-
-
-
-
-
-
-
 
 file_types = (('JPG IMAGE', '*.jpg'), ('PNG IMAGE', '*.png'))
-lith_type_frame_layout = [[sg.Radio('NightLight', "lith_type", default=True, size=(10,1), k='-R1-'), sg.Radio('Window Cling', "lith_type", default=False, size=(10,1), k='-R2-')]]
-size_frame_layout = [[sg.pin(sg.Radio('Small', "size", default=True, key='-small-')), sg.pin(sg.Radio('Medium', "size", default=False, key='-medium-')), sg.pin(sg.Radio('Large', "size", default=False, key='-large-'))],
-                     [sg.pin(sg.Radio('Square', "ori", default=True, key='-square-')), sg.pin(sg.Radio('Portrait', "ori", default=False, key='-portrait-')), sg.pin(sg.Radio('Landscape', "ori", default=False, key='-landscape-'))]]
 
-load_img_frame_layout = [[sg.Text('File: '), sg.Text('', key='-filename-')],
-                         [sg.FileBrowse(file_types=file_types)]]
+lith_type_frame_layout = [[sg.Radio('NightLight', "lith_type", default=True, size=(10,1), k='-nightlight-'), sg.Radio('Window Cling', "lith_type", default=False, size=(10,1), k='-window-')]]
+
+sub_type_frame_layout = [[sg.pin(sg.Radio('Small', "size", default=True, key='-small-')), sg.pin(sg.Radio('Medium', "size", default=False, key='-medium-')), sg.pin(sg.Radio('Large', "size", default=False, key='-large-'))],
+                         [sg.pin(sg.Radio('Square', "ori", default=True, key='-square-')), sg.pin(sg.Radio('Portrait', "ori", default=False, key='-portrait-')), sg.pin(sg.Radio('Landscape', "ori", default=False, key='-landscape-'))]]
+
+image_preview_frame_layout = [
+    [sg.Image(key="-IMAGE-")]
+]
+
+file_input_frame_layout = [[sg.Text('File: '), sg.Text('', key='-filename-')],
+                           [sg.FileBrowse(file_types=file_types)]]
+dl_path_frame_layout = [[sg.Text('Download Path: '), sg.Text('', key='-dl_path-')],
+                         [sg.FolderBrowse(initial_folder='c:/users')]]
 layout = [[sg.Frame('Lithophane Type', lith_type_frame_layout)],
-          [sg.pin(sg.Frame('Size', size_frame_layout, key='sz'))],
-          [sg.Frame('Load Image', load_img_frame_layout)],
+          [sg.pin(sg.Frame('Size', sub_type_frame_layout, key='sz'))],
+          [sg.Frame('', dl_path_frame_layout)],
+          [sg.Frame('Load Image', file_input_frame_layout)],
+          [sg.Frame('Preview', image_preview_frame_layout, size=(200,200))],
+
           [sg.Button('Ok'),  sg.Button('Cancel')]]
 
 window = sg.Window('Lithophane Setup', layout)
@@ -132,16 +128,46 @@ window = sg.Window('Lithophane Setup', layout)
 while True:
     event, values = window.read(timeout=100)
     filename_elem = window['-filename-']
-    print(event, values)
+    dl_path_elem = window['-dl_path-']
+
+
+
+    if values['-nightlight-'] and values['-square-']:
+        base_type = 'nl'
+        sub_type = 'square'
+    elif values['-nightlight-'] and values['-portrait-']:
+        base_type = 'nl'
+        sub_type = 'portrait'
+    elif values['-nightlight-'] and values['-landscape-']:
+        base_type = 'nl'
+        sub_type = 'landscape'
+    elif values['-window-'] and values['-small-']:
+        base_type = 'wind'
+        sub_type = 'sm'
+    elif values['-window-'] and values['-medium-']:
+        base_type = 'wind'
+        sub_type = 'med'
+    elif values['-window-'] and values['-large-']:
+        base_type = 'wind'
+        sub_type = 'lrg'
+
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
-
 
     if values['Browse'] != '':
         fp = os.path.normpath(values['Browse'])
         fn = os.path.basename(fp)
         filename_elem.update(fn)
-    if values['-R2-']:
+        if os.path.exists(fp):
+            image = Image.open(values["Browse"])
+            image.thumbnail((200, 200))
+            bio = io.BytesIO()
+            image.save(bio, format="PNG")
+            window["-IMAGE-"].update(data=bio.getvalue())
+    if values['Browse0'] != '':
+        dl_path = os.path.normpath(values['Browse0'])
+        dl_path_elem.update(dl_path)
+    if values['-window-']:
         window['-small-'].update(visible=True)
         window['-medium-'].update(visible=True)
         window['-large-'].update(visible=True)
@@ -149,7 +175,7 @@ while True:
         window['-portrait-'].update(visible=False)
         window['-landscape-'].update(visible=False)
 
-    if values['-R1-']:
+    if values['-nightlight-']:
         window['-small-'].update(visible=False)
         window['-medium-'].update(visible=False)
         window['-large-'].update(visible=False)
@@ -157,10 +183,15 @@ while True:
         window['-portrait-'].update(visible=True)
         window['-landscape-'].update(visible=True)
 
-    if event == 'Ok':
-        my_lith = Lithophane(template, f'{template}_{size}')
-        my_lith.dl_nl(fp)
 
-        break
+
+    if event == 'Ok':
+        my_lith = Lithophane(base_type, f'{base_type}_{sub_type}')
+        if base_type == 'nl':
+            my_lith.dl_nl(fp, dl_path)
+        else:
+            my_lith.dl_wind(fp, dl_path)
+
+        # break
 
 window.close()
