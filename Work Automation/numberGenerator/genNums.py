1c8d403b2abf3a44c842c79bb6fdd03b6d500232
Troy P <troypouliot@yahoo.com>
2021-12-17 11:05:25 -0600

    Add error checking

--- Work Automation/numberGenerator/genNums.py
+++ Work Automation/numberGenerator/genNums.py
@@ -3,21 +3,30 @@ import random
 
 with open("used_nums.json", 'r') as f:
     used_nums = list(json.load(f))
-
+print(used_nums)
 available_nums = []
 
 for i in range(10000, 99999):
     if i not in used_nums:
         available_nums.append(i)
 
-num_of_nums = int(input('How many new numbers do you want? '))
+num_of_nums = None
+while type(num_of_nums) != int:
+    try:
+        num_of_nums = int(input('How many new numbers do you want? '))
+    except ValueError:
+        print('That is not a number')
 
-new_set = random.choices(available_nums, k=num_of_nums)
+if num_of_nums < 1:
+    print('Goodbye!')
 
-used_nums.append(new_set)
-with open("used_nums.json", 'w') as f:
-    # indent=2 is not needed but makes the file human-readable
-    json.dump(used_nums, f, indent=2)
+else:
 
+    new_set = random.choices(available_nums, k=num_of_nums)
 
-print('New set of numbers: {}'.format(new_set))
+    used_nums.append(new_set)
+    with open("used_nums.json", 'w') as f:
+        # indent=2 is not needed but makes the file human-readable
+        json.dump(used_nums, f, indent=2)
+
+    print('New set of numbers: {}'.format(new_set))
